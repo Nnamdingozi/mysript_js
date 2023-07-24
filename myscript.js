6b2756e2adf12851ab5abe10e566dd07b5e7924e
joerald <ngozika1105@gmail.com>
2023-07-24 15:18:39 +0100

    add function for generating random numbers

--- myscript.js
+++ myscript.js
@@ -1,10 +1,13 @@
-let months  = ["January","February","March","April", "May","June",
-         "July", "August", "September", "October", "November", "December",];
+let months = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December",];
 
- let  zodiacSign = ["Cancer", "Gemini","Taurus","Aries", "Pisces","Aquarius","Capricorn",
-         "Sagritarius","Sagittarius","Scopius","Libra","Virgo", "Leo",];
+let zodiacSign = ["Cancer", "Gemini", "Taurus", "Aries", "Pisces", "Aquarius", "Capricorn",
+        "Sagritarius", "Sagittarius", "Scopius", "Libra", "Virgo", "Leo",];
 
-let  advice = ["take a walk at the beach","express your thoughts to others","be active and flexible",
-         "take a break from your usual emotional intensity", 
-         "get your heart rate up and pumping", "engage in teram sports" ];
-         
+let advice = ["take a walk at the beach", "express your thoughts to others", "be active and flexible",
+        "take a break from your usual emotional intensity",
+        "get your heart rate up and pumping", "engage in teram sports"];
+const getRandNum = (arr) => {
+        return Math.floor(Math.random() * arr.length)
+}
+console.log(getRandNum(months));
